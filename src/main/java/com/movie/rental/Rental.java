1bca406c0ce079e5c21ead2b3dcad99a1fcdc272
Kaushal <kaushalraok@gmail.com>
2023-10-06 16:56:01 +0530

    Fix code smell and extract amount and frequentRenterPoints

--- src/main/java/com/movie/rental/Rental.java
+++ src/main/java/com/movie/rental/Rental.java
@@ -1,8 +1,8 @@
 package com.movie.rental;
 
 public class Rental {
-    private int daysRented;
-    private Movie movie;
+    private final int daysRented;
+    private final Movie movie;
 
     public Rental(Movie movie, int daysRented) {
         this.movie = movie;
@@ -15,5 +15,32 @@ public class Rental {
 
     public Movie getMovie() {
         return movie;
+    }
+
+    public double amount() {
+        double amount = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (daysRented > 2)
+                    amount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount += 1.5;
+                if (daysRented > 3)
+                    amount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
+
+    public int frequentRenterPoints() {
+        int points = 1;
+        // add bonus for a two days new release rental
+        if ((movie.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) points++;
+        return points;
     }
 }
--- src/test/java/com/movie/rental/CustomerTests.java
+++ src/test/java/com/movie/rental/CustomerTests.java
@@ -2,9 +2,25 @@ package com.movie.rental;
 
 import org.junit.jupiter.api.Test;
 
-class CustomerTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class CustomerTest {
     @Test
-    public void test() {
+    void shouldGenerateStatement() {
+        Customer customer = new Customer("ABC");
+        customer.addRental(new Rental(new Movie("Movie-1", Movie.REGULAR), 4));
+        customer.addRental(new Rental(new Movie("Movie-2", Movie.REGULAR), 1));
+        customer.addRental(new Rental(new Movie("Movie-3", Movie.CHILDRENS), 5));
+        customer.addRental(new Rental(new Movie("Movie-4", Movie.CHILDRENS), 2));
+        customer.addRental(new Rental(new Movie("Movie-5", Movie.NEW_RELEASE), 3));
+
+        assertEquals("Rental Record for ABC\n" +
+                "\tMovie-1\t5.0\n" +
+                "\tMovie-2\t2.0\n" +
+                "\tMovie-3\t4.5\n" +
+                "\tMovie-4\t1.5\n" +
+                "\tMovie-5\t9.0\n" +
+                "Amount owed is 22.0\n" +
+                "You earned 6 frequent renter points", customer.statement());
     }
 }
